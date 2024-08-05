872e45a542ffadae019a93c8c8a9bc096e567106
Remi Caput <979446+r3c@users.noreply.github.com>
2024-08-05 14:56:56 +0200

    [network.http] Allow specifying cookies array. (#7)

--- test/network/http.php
+++ test/network/http.php
@@ -8,7 +8,11 @@ function assert_message($message, $code = null, $data = null, $headers = array()
     assert($message !== null, 'Message is null');
 
     if ($data !== null) {
-        assert(preg_match($data, $message->data), 'Data is "' . $message->data . '", does not match "' . $data . '"');
+        if (is_array($data)) {
+            assert_json_include($data, json_decode($message->data, true), '');
+        } else {
+            assert(preg_match($data, $message->data), 'Message must match expression "' . $data . '" but was: ' . $message->data);
+        }
     }
 
     if ($code !== null) {
@@ -22,18 +26,37 @@ function assert_message($message, $code = null, $data = null, $headers = array()
     }
 }
 
+function assert_json_include($reference, $candidate, $path)
+{
+    if (is_array($reference)) {
+        foreach ($reference as $key => $value) {
+            assert(isset($candidate[$key]), 'Value in path "' . $path . '" must have key "' . $key . '" set but was: ' . json_encode($candidate));
+            assert_json_include($value, $candidate[$key], $path . '.' . $key);
+        }
+    } else {
+        assert($candidate === $reference, 'Value in path "' . $path . '" must be "' . $reference . '" but was: ' . $candidate);
+    }
+}
+
 header('Content-Type: text/plain');
 
 ini_set('assert.exception', true);
+ini_set('html_errors', false);
 
 $http = new HTTP();
 
-assert_message($http->query('GET', 'http://httpbin.org/get?key=value'), 200, '/"key": "value"/');
+assert_message($http->query('GET', 'http://httpbin.org/get?key=value'), 200, array('args' => array('key' => 'value')));
 assert_message($http->query('GET', 'http://httpbin.org/headers'), 200, null, array('content-type' => 'application/json', 'Content-Type' => 'application/json'));
 assert_message($http->query('GET', 'http://httpbin.org/image/png'), 200, null, array('content-type' => 'image/png'));
-assert_message($http->query('POST', 'http://httpbin.org/post', array('key' => 'value')), 200, '/"key": "value"/');
+assert_message($http->query('POST', 'http://httpbin.org/post', array('key' => 'value')), 200, array('form' => array('key' => 'value')));
 assert_message($http->query('GET', 'http://httpbin.org/status/408'), 408, null);
-assert_message($http->query('GET', 'invalid'), 0, '//');
+assert_message($http->query('GET', 'invalid'), 0, null);
+
+$http->cookies = array('a' => 'x', 'b' => 'y');
+
+assert_message($http->query('GET', 'https://httpbin.org/cookies'), 200, array('cookies' => array('a' => 'x', 'b' => 'y')));
+
+$http->cookies = null;
 
 assert_message(HTTP::code(302), 302);
 assert_message(HTTP::code(404, 'test'), 404, '/test/');
